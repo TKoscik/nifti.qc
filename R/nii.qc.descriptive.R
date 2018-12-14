e1d141e771c2afa983dd9aa3f70ce16d5fbaea8b
TKoscik <timkoscik@gmail.com>
2018-12-14 12:06:32 -0600

    renaming vars

--- R/nii.qc.descriptive.R
+++ R/nii.qc.descriptive.R
@@ -9,24 +9,24 @@ nii.qc.descriptive <- function(
   summary <- list()
   summary$stats <- data.frame(
     mean = mean(img, na.rm=TRUE),
-    sd <- sd(img, na.rm=TRUE),
-    median <- median(img, na.rm=TRUE),
-    mad <- mad(img, na.rm=TRUE),
-    skew <- skewness(img, na.rm=TRUE),
-    kurtosis <- kurtosis(img, na.rm=TRUE),
-    q05 <- quantile(img, 0.05, na.rm=TRUE),
-    q95 <- quantile(img, 0.95, na.rm=TRUE))
+    sd = sd(img, na.rm=TRUE),
+    median = median(img, na.rm=TRUE),
+    mad = mad(img, na.rm=TRUE),
+    skew = skewness(img, na.rm=TRUE),
+    kurtosis = kurtosis(img, na.rm=TRUE),
+    q05 = quantile(img, 0.05, na.rm=TRUE),
+    q95 = quantile(img, 0.95, na.rm=TRUE))
   df <- data.frame(Intensity=img)
   summary$plot <- ggplot(df, aes(x=Intensity)) +
     theme_bw() +
     geom_density(size=1, fill="#646464", color="transparent") +
     labs(x="Intensity", y="Density") +
-    geom_vline(xintercept=summary$mean, linetype="dotted", size=1) +
+    geom_vline(xintercept=summary$stats$mean, linetype="dotted", size=1) +
     annotate(geom = "rect",
-             xmin=summary$mean - summary$sd,
-             xmax=summary$mean + summary$sd,
+             xmin=summary$stats$mean - summary$stats$sd,
+             xmax=summary$stats$mean + summary$stats$sd,
              ymin=-Inf, ymax=Inf, alpha=0.15, color="transparent") +
-    annotate(geom = "rect", xmin=summary$q05, xmax=summary$q95,
+    annotate(geom = "rect", xmin=summary$stats$q05, xmax=summary$q95,
              ymin=-Inf, ymax=Inf, alpha=0.15, color="transparent")
   return(summary)
 }
